bd8e737a69772e4ec52c850360849a6a3dc60d87
Pukar Chhatkuli <dev.pukar.chhatkuli@gmail.com>
2025-08-24 18:50:35 +0545

    non-primitive data type(list, dictionary), function(return, argument, parameter),

---
+++ 2_data-types/non-primitive/main.py
@@ -0,0 +1,16 @@
+# List
+results = {
+  0: "Ashvik",
+  1: "Mahesh",
+  2: "Kritik",
+}
+print(results[1])
+
+# Dictionary
+characters = {
+  "weight": 45,
+  "height": 200,
+  0: 12,
+  "hair_color": "black",
+}
+print(characters[0])
