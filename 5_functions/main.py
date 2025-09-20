b20016b479f7f93b4f9a9a88951f5f51cbd743e4
Pukar Chhatkuli <dev.pukar.chhatkuli@gmail.com>
2025-09-20 19:51:29 +0545

    Refactor main.py and notes.txt to implement SetOperation class and item counting logic

--- 5_functions/main.py
+++ 5_functions/main.py
@@ -1,23 +1,3 @@
-set_a = {4, 5, 11, 5, 13}
-set_b = {5, 11, 16, 17}
-set_c = {15, 16, 18}
-
-class SetOperation:
-  def find_intersection(self, *arg):
-    print(arg)
-    match len(arg):
-      case 2:
-        for i in range(10): # earth (15000)
-          star = ""
-          for _ in range(i):
-            star += " *"
-          print(star)
-      case 3:
-        print("three")
-
-result = SetOperation()
-result.find_intersection(set_a, set_b)
-# result.find_intersection(set_a, set_b, set_c)
-# *
-# * *
-# * * *
+count = 1
+while count <= 10:
+  print(count)
